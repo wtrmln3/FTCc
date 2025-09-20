eb716982fd60a8ffb1d5eb99b949b52031583ccc
Lenovo <d94734907@gmail.com>
2025-09-20 18:26:56 +0500

    Initial commit

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Commands/ForwardTakeServos.java
@@ -0,0 +1,30 @@
+package org.firstinspires.ftc.teamcode.Commands;
+
+import com.arcrobotics.ftclib.command.CommandBase;
+import org.firstinspires.ftc.teamcode.Subsystems.TakeServos;
+
+    public class ForwardTakeServos extends CommandBase {
+        private final TakeServos takeServos;
+
+        public ForwardTakeServos(TakeServos takeServos) {
+            this.takeServos = takeServos;
+
+            addRequirements(takeServos);
+        }
+
+        @Override
+        public void initialize() {
+            takeServos.forward();
+        }
+
+        @Override
+        public void end(boolean interrupted) {
+            takeServos.stop();
+        }
+
+        @Override
+        public boolean isFinished() {
+            return false;
+        }
+    }
+
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/TeleOpRobot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/TeleOpRobot.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.OpModes;
+/*package org.firstinspires.ftc.teamcode.OpModes;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
@@ -61,3 +61,4 @@ public class TeleOpRobot extends LinearOpMode {
     }
 
 }
+*/
